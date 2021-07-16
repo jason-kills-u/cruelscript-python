f6c996f5b34c5d66226ea7f8a1c6927e717cc556
jason-kills-u <75362028+jason-kills-u@users.noreply.github.com>
2021-07-16 09:47:48 +0700

    A few optimizing changes
    Added more ways to end cruelscript.

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from interpret import interpret
 argc = len(argv) - 1
 
 if argc < 1:
-	print("CruelScript Version 1.3.0, New Version Every Weeks")
-	while(True):
-		G = input(">>> ")
-		if G == 'exit':
-			break
-		interpret(G)
-		print(end = "\n")
+	print("CruelScript Version 1.3.1 Final.")
+	G = input(">>> ")
+	if G == 'exit' or "quit" or "x" or "X":
+		break
+	interpret(G)
+	print(end = "\n")
 else:
 	h = open(argv[1], "r")
 	interpret(h.read())
